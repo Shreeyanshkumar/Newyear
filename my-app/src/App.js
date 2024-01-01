86a2a9af00e100fb28da2ae1dfe333aa9974135a
Shreeyanshkumar <shreeyanshk.ug21.ec@nitp.ac.in>
2024-01-02 01:18:50 +0530

    designing webpage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [fieldValue, setFieldValue] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cann't Tell Them directly?</h1>
+      <h1>Fikar Not</h1>
+      <h2>We are here to help you</h2>
+      <h1>Just drop Your Feelings for your love.</h1>
+      <h2>And the best part is we wont know your name even.</h2>
+
+      <input type="text" name="" id="" value={fieldValue} onChange={(e) => setFieldValue(e.target.value)} />
+
+      {fieldValue}
     </div>
   );
 }
